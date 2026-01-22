b2e9212dad87e19ded4a747d7839a1a1bfc4209a
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2026-01-22 23:16:40 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// config.js  (TESTING ONLY - repo private রাখো)
 export const BOT_TOKEN = "7985415284:AAGtJ7CMy783_N2xq5hXuC8PkcPpdx2nCYQ";
 export const TARGET_CHAT_ID = "-1003591318904";
-export const BOT_USERNAME = "terihasarbot"; // without @
+export const BOT_USERNAME = "terihasarbot";
 
-// 30 seconds "requested" (Cron limitation: 30-90s)
-export const DELETE_AFTER_SECONDS = 30;
+// এখন 10 সেকেন্ড রাখো (বা 5)
+export const DELETE_AFTER_SECONDS = 10;
 
 export const DELETE_NOTICE_TEXT =
-  "⚠️ টেলিগ্রামের কপিরাইট/নিরাপত্তা নীতির কারণে ফাইলটি ৩০ সেকেন্ডের বেশি রাখা যায় না।\n\nপূর্বের ফাইল পেতে আবার রিকোয়েস্ট করুন।";
+  "⚠️ টেলিগ্রামের কপিরাইট/নিরাপত্তা নীতির কারণে ফাইলটি অল্প সময়ের বেশি রাখা যায় না।\n\nপূর্বের ফাইল পেতে আবার রিকোয়েস্ট করুন।";
